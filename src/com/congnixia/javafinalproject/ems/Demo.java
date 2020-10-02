b91ecf8b683fa128346bcaa03b4aa4666468ef80
liamcclane <lia_mcclane@msn.com>
2020-10-02 09:46:54 -0700

    cleaned comments in main demo

--- src/com/congnixia/javafinalproject/ems/Demo.java
+++ src/com/congnixia/javafinalproject/ems/Demo.java
@@ -1,5 +1,6 @@
 package com.congnixia.javafinalproject.ems;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import com.congnixia.javafinalproject.ems.models.*;
@@ -11,71 +12,13 @@ public class Demo {
 
 	static Department dummyDepartment = new Department("ExDep", 111, 3000.00d);
 	static Employee dummyEmployee = new Employee("ExEmp", "dummy@dummy.com", "234-234-2344", "4/3/2020", dummyDepartment);
+	static String userInput;
+	static ArrayList<Employee> employees;
+	static Employee employee;
+	static Department department;
 	
 	public static void main(String[] args) {
-
 		start();
-		/**
-		 * 
-		 * goal is to demo our EMS for Best Boughts Store
-		 * 
-		 * when we run this file,
-		 * 
-		 * greet user, ask what are we doing if we are adding DB (Either Employee or
-		 * Department) looking for some information that is already stored
-		 * 
-		 * partting
-		 * 
-		 */
-
-		/**
-		 * 
-		 * if we are adding prompt user, what are we adding? Employee, or opening a new
-		 * department
-		 * 
-		 * If employee, prompt for information name Department they are going into
-		 * output the newly made employee back to the user
-		 * 
-		 * then loop back to the top
-		 * 
-		 * 
-		 * 
-		 */
-
-		/**
-		 * 
-		 * if we are looking some one up
-		 * 
-		 * output to user the filds they can search by
-		 * 
-		 * id, name, department,
-		 * 
-		 * user will type in "name" or "id" if name "who are we looking for" if id "2"
-		 * // if extra time maybe take "10 (though) 20" as valid
-		 * 
-		 * output either the single use they are looking for or the list of people
-		 * 
-		 * if it was a list a people, ask for a number from the user for which person
-		 * was the correct one. output that Employee to the user
-		 * 
-		 * if only one person matches what they are looking for output the Employee to
-		 * the user
-		 * 
-		 * 
-		 * Prompt yes or no if they would like to update them
-		 * 
-		 */
-
-		/**
-		 * 
-		 * if the user wants to update the Employee ask them which field they want to
-		 * change name, department "you are changing {{Employee}}'s {{field}}
-		 * information" "what would you like to change it to?"
-		 * 
-		 * "confirmation message" {{updated employees info}}
-		 * 
-		 */
-
 	}
 
 	public static void start() {
